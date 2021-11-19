daecef17e01c30fa0b56e874bcb529f6e650817f
Luke <luke.esterhuizen@yamasoftware.co.za>
2021-11-19 10:05:13 +0200

    Uluntu p1 - register screen - removed some fields

--- lib/pages/register.dart
+++ lib/pages/register.dart
@@ -12,125 +12,192 @@ class _RegisterState extends State<Register> {
 
   final _formKey = GlobalKey<FormState>();
 
+  var _autovalidate = false;
+  var _name;
+  var _surname;
+  var _email;
+
+  bool isCheckedTc = false;
+  bool isCheckedPopia = false;
+
   @override
   Widget build(BuildContext context) {
+    Color getColor(Set<MaterialState> states) {
+      const Set<MaterialState> interactiveStates = <MaterialState>{
+        MaterialState.pressed,
+        MaterialState.hovered,
+        MaterialState.focused,
+      };
+      if (states.any(interactiveStates.contains)) {
+        return Colors.blue;
+      }
+      return Colors.red;
+    }
+
     return Scaffold(
         backgroundColor: Colors.white,
         appBar: AppBar(
           backgroundColor: myColor,
         ),
-        body: Padding(
-          padding: EdgeInsets.all(30.0),
-          child: Column(
-            children: <Widget>[
-              Image(
-                image: AssetImage('assets/uluntu-logo-300.png'),
-                height: 110.0,
-              ),
-              SizedBox(
-                height: 30.0,
-              ),
-              Text(
-                'Welcome to Uluntu',
-                style: TextStyle(
-                  fontSize: 20.0,
-                  fontWeight: FontWeight.bold,
+        body: SingleChildScrollView(
+          child: Padding(
+            padding: EdgeInsets.all(30.0),
+            child: Column(
+              children: <Widget>[
+                Image(
+                  image: AssetImage('assets/uluntu-logo-300.png'),
+                  height: 110.0,
                 ),
-              ),
-              SizedBox(
-                height: 5.0,
-              ),
-              Text(
-                'Some catchy phrase can come here',
-                style: TextStyle(
-                  fontSize: 14.0,
-                  fontWeight: FontWeight.bold,
+                SizedBox(
+                  height: 30.0,
                 ),
-              ),
-              SizedBox(
-                height: 10.0,
-              ),
-              Form(
-                  child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  SizedBox(
-                    height: 20.0,
+                Text(
+                  'Welcome to Uluntu',
+                  style: TextStyle(
+                    fontSize: 20.0,
+                    fontWeight: FontWeight.bold,
                   ),
-                  Text('Name'),
-                  SizedBox(
-                    height: 5.0,
+                ),
+                SizedBox(
+                  height: 5.0,
+                ),
+                Text(
+                  'Some catchy phrase can come here',
+                  style: TextStyle(
+                    fontSize: 14.0,
+                    fontWeight: FontWeight.bold,
                   ),
-                  TextFormField(
-                    style: TextStyle(height: 0.6),
-                    decoration: const InputDecoration(
-                      border: OutlineInputBorder(),
-                    ),
-                    // The validator receives the text that the user has entered.
-                    validator: (value) {
-                      if (value == null || value.isEmpty) {
-                        return 'Please enter some text';
-                      }
-                      return null;
-                    },
-                  ),
-                  SizedBox(
-                    height: 40.0,
-                  ),
-                  Text('ID Number'),
-                  SizedBox(
-                    height: 5.0,
-                  ),
-                  TextFormField(
-                    style: TextStyle(height: 0.6),
-                    decoration: const InputDecoration(
-                      border: OutlineInputBorder(),
-                    ),
-                    // The validator receives the text that the user has entered.
-                    validator: (value) {
-                      if (value == null || value.isEmpty) {
-                        return 'Please enter some text';
-                      }
-                      return null;
-                    },
-                  ),
-                  SizedBox(
-                    height: 40.0,
-                  ),
-                  Text('Cell Number'),
-                  SizedBox(
-                    height: 5.0,
-                  ),
-                  TextFormField(
-                    style: TextStyle(height: 0.6),
-                    decoration: const InputDecoration(
-                      border: OutlineInputBorder(),
-                    ),
-                    // The validator receives the text that the user has entered.
-                    validator: (value) {
-                      if (value == null || value.isEmpty) {
-                        return 'Please enter some text';
-                      }
-                      return null;
-                    },
-                  ),
-                  SizedBox(
-                    height: 40.0,
-                  ),
-                  TextButton(
-                    onPressed: () {
-                      Navigator.pushNamed(context, '/verify_otp');
-                    },
-                    child: const Text('Register'),
-                    style: TextButton.styleFrom(
-                      backgroundColor: pink,
-                      primary: Colors.white,
-                      minimumSize: Size(double.infinity, 40),
-                    ),
-                  )
-                ],
-              ))
-            ],
+                ),
+                SizedBox(
+                  height: 10.0,
+                ),
+                Form(
+                    autovalidate: _autovalidate,
+                    key: _formKey,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        SizedBox(
+                          height: 20.0,
+                        ),
+                        Text('Name'),
+                        SizedBox(
+                          height: 5.0,
+                        ),
+                        TextFormField(
+                            style: TextStyle(height: 0.6),
+                            decoration: const InputDecoration(
+                              border: OutlineInputBorder(),
+                            ),
+                            onSaved: (value) {
+                              _name = value;
+                            },
+                            validator: (value) {
+                              if (value!.isEmpty) {
+                                return 'Please enter your username';
+                              }
+                              return null;
+                            }),
+                        SizedBox(
+                          height: 40.0,
+                        ),
+                        Text('Surname'),
+                        SizedBox(
+                          height: 5.0,
+                        ),
+                        TextFormField(
+                            style: TextStyle(height: 0.6),
+                            decoration: const InputDecoration(
+                              border: OutlineInputBorder(),
+                            ),
+                            onSaved: (value) {
+                              _surname = value;
+                            },
+                            validator: (value) {
+                              if (value!.isEmpty) {
+                                return 'Please enter your username';
+                              }
+                              return null;
+                            }),
+                        SizedBox(
+                          height: 40.0,
+                        ),
+                        Text('Email'),
+                        SizedBox(
+                          height: 5.0,
+                        ),
+                        TextFormField(
+                          validator: (val) => val!.isEmpty || !val.contains("@")
+                              ? "enter a valid eamil"
+                              : null,
+                          style: TextStyle(height: 0.6),
+                          decoration: const InputDecoration(
+                            border: OutlineInputBorder(),
+                          ),
+                          onSaved: (value) {
+                            _email = value;
+                          },
+                        ),
+                        SizedBox(
+                          height: 40.0,
+                        ),
+                        Row(
+                          children: <Widget>[
+                            Text('Accept terms and conditions'),
+                            Checkbox(
+                              checkColor: Colors.white,
+                              fillColor:
+                                  MaterialStateProperty.resolveWith(getColor),
+                              value: isCheckedTc,
+                              onChanged: (bool? value) {
+                                setState(() {
+                                  isCheckedTc = value!;
+                                });
+                              },
+                            ),
+                          ],
+                        ),
+                        Row(
+                          children: <Widget>[
+                            Text('Accept POPIA'),
+                            Checkbox(
+                              checkColor: Colors.white,
+                              fillColor:
+                                  MaterialStateProperty.resolveWith(getColor),
+                              value: isCheckedPopia,
+                              onChanged: (bool? value) {
+                                setState(() {
+                                  isCheckedPopia = value!;
+                                });
+                              },
+                            ),
+                          ],
+                        ),
+                        TextButton(
+                          onPressed: () {
+                            Navigator.pushNamed(context, '/verify_otp');
+                            // if (_formKey.currentState!.validate()) {
+                            //   _formKey.currentState!.save();
+                            //   print(_name);
+                            //   print(_password);
+                            //   print(_confirmPassword);
+                            // } else {
+                            //   setState(() {
+                            //     _autovalidate = true;
+                            //   });
+                            // }
+                          },
+                          child: const Text('Register'),
+                          style: TextButton.styleFrom(
+                            backgroundColor: pink,
+                            primary: Colors.white,
+                            minimumSize: Size(double.infinity, 40),
+                          ),
+                        )
+                      ],
+                    ))
+              ],
+            ),
           ),
         ));
   }
